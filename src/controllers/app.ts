c6c46b7b976b2e6467ad8c91437184b8114dc63f
Ted Shaffer <shaffer.family@gmail.com>
2022-03-13 07:14:18 -0700

    getWords feature complete.

--- src/controllers/app.ts
+++ src/controllers/app.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 const en = require('dictionary-en');
 import * as fs from 'fs';
+import { isNil } from 'lodash';
 
 import { version } from '../version';
 
@@ -20,13 +21,6 @@ export const initializeSpellChecker = () => {
 
   // Load a dictionary
   spellchecker.use(DICT);
-
-  // Check a word
-  const isPizzaRight = spellchecker.check('pizza');
-  const isFlibbetRight = spellchecker.check('flibbet');
-
-  console.log('isPizzaRight: ' + isPizzaRight);
-  console.log('isFlibbetRight: ' + isFlibbetRight);
 
   // en(function (err: any, result: any) {
   //   console.log(err || result);
@@ -48,31 +42,49 @@ export const getWords = (request: Request, response: Response, next: any) => {
 
   const { candidateLettersAtLocation, lettersNotAtExactLocation } = request.body;
 
-  // for (let letterIndex = 0; letterIndex < 5; letterIndex++) {
-    for (let clalIndex0 = 0; clalIndex0 < candidateLettersAtLocation[0].length; clalIndex0++) {
-      const clal0 = candidateLettersAtLocation[0][clalIndex0];
-      for (let clalIndex1 = 0; clalIndex1 < candidateLettersAtLocation[1].length; clalIndex1++) {
-        const clal1 = candidateLettersAtLocation[1][clalIndex1];
-        for (let clalIndex2 = 0; clalIndex2 < candidateLettersAtLocation[2].length; clalIndex2++) {
-          const clal2 = candidateLettersAtLocation[2][clalIndex2];
-          for (let clalIndex3 = 0; clalIndex3 < candidateLettersAtLocation[3].length; clalIndex3++) {
-            const clal3 = candidateLettersAtLocation[3][clalIndex3];
-            for (let clalIndex4 = 0; clalIndex4 < candidateLettersAtLocation[4].length; clalIndex4++) {
-              const clal4 = candidateLettersAtLocation[4][clalIndex4];
-              
-              // check word
-              const candidateWord = clal0 + clal1 + clal2 + clal3 + clal4;
+  const words: string[] = [];
+
+  for (let clalIndex0 = 0; clalIndex0 < candidateLettersAtLocation[0].length; clalIndex0++) {
+    const clal0 = candidateLettersAtLocation[0][clalIndex0];
+    for (let clalIndex1 = 0; clalIndex1 < candidateLettersAtLocation[1].length; clalIndex1++) {
+      const clal1 = candidateLettersAtLocation[1][clalIndex1];
+      for (let clalIndex2 = 0; clalIndex2 < candidateLettersAtLocation[2].length; clalIndex2++) {
+        const clal2 = candidateLettersAtLocation[2][clalIndex2];
+        for (let clalIndex3 = 0; clalIndex3 < candidateLettersAtLocation[3].length; clalIndex3++) {
+          const clal3 = candidateLettersAtLocation[3][clalIndex3];
+          for (let clalIndex4 = 0; clalIndex4 < candidateLettersAtLocation[4].length; clalIndex4++) {
+            const clal4 = candidateLettersAtLocation[4][clalIndex4];
+
+            const candidateWord = clal0 + clal1 + clal2 + clal3 + clal4;
+
+            // ensure that word contains all lettersNotAtExactLocation
+            let allLettersNotAtExactLocationAreInThisWord = true;
+            const candidateWordAsArray = candidateWord.split('');
+            for (const letterNotAtExactLocation of lettersNotAtExactLocation) {
+              if (!isNil(letterNotAtExactLocation)) {
+                if (candidateWordAsArray.indexOf(letterNotAtExactLocation) < 0) {
+                  // console.log(letterNotAtExactLocation + ' is not anywhere in ' + candidateWord);
+                  allLettersNotAtExactLocationAreInThisWord = false;
+                  break;
+                }
+              }
+            }
+
+            if (allLettersNotAtExactLocationAreInThisWord) {
               const isWord = spellchecker.check(candidateWord);
-              console.log(candidateWord + ' ' + isWord);
+              // console.log(candidateWord + ' ' + isWord);
+              if (isWord) {
+                words.push(candidateWord);
+              }
             }
           }
         }
       }
     }
-  // }
+  }
 
   response.status(200).json({
     success: true,
-    words: ['herro', 'pizza'],
+    words,
   });
 };
